251d2d1e3271afbc76f29a59d587952c64528d6a
MunaLombe <3223352545@qq.com>
2023-02-21 00:40:43 +0300

    filter is almost doen. The only remaining part is the range selector's output value fields.

--- src/components/FilterSearchResults.jsx
+++ src/components/FilterSearchResults.jsx
@@ -5,7 +5,7 @@ import RangeSelector from "./rangSelector/RangeSelector"
 const FilterSearchResults = ({ children }) => {
 
 
-
+  
   const Categories =({categories={mainTitle:"Category", categoryTypes:[1,2,3,4]},children})=>{
     return(
       <div className="categories-wrapper   flex flex-col gap-1   ">
@@ -15,7 +15,7 @@ const FilterSearchResults = ({ children }) => {
         <div className="category-types-wrapper font-medium">
           {
             categories.categoryTypes.map((i,x)=>
-            <div className="category-type">
+            <div className="category-type hover:text-blue-500 cursor-pointer">
               {"Cate"}
             </div>
             )
@@ -26,20 +26,20 @@ const FilterSearchResults = ({ children }) => {
   }
   const FilterInput = ({ inputType = "toggle", inputData = { label: "Super Sale", options: [{ type: "regular", text: "opt1" }, { type: "sameDay", text: "opt2", hasIco: true }, { type: "express", text: "opt3", hasIco: true }, { type: "regular", text: "opt4" }]}, children})=>{
     const TogleOpt=({data, type="checkbox"})=>(
-      <div className="toggle-wrapper flex justify-between child:w-max whitespace-nowrap font-semibold ">
+      <div className="toggle-wrapper flex justify-between whitespace-nowrap font-semibold ">
         <label htmlFor="toggleBtn" >{data.label}</label>
-        <ToggleIco inputName="toggleBtn" type={type}/>
-  
+          <ToggleIco inputName="toggleBtn" type={type}/>
       </div>
     )
+    // checkbox: appearance-none
     const SelectOpt = ({data}) => (
       <div className="select-wrapper flex flex-col whitespace-nowrap ">
         <div className="h4 selector-title font-semibold">{data.label}</div>
         <div className="selectors-wrapper flex flex-col font-medium">
           {
             data.options.map((opt,x)=>
-              <div key={x} className="select-option-item  flex flex-row gap-2">
-                <input type="checkbox" name="toggleBtn" id="" className="border-1 border-slate-300 " />
+              <div key={x} className="select-option-item  flex flex-row items-baseline gap-2">
+                <input type="checkbox" name="toggleBtn" id="" className={"appearance-none w-[16px] aspect-square border border-slate-300 hover:border-blue-500  checked:w-[14px] checked:mx-[0.999px] checked:bg-blue-500 checked:outline checked:outline-1 checked:outline-blue-500  checked:outline-offset-2  rounded-[4px] cursor-pointer" } />
                 <label htmlFor="toggleBtn">
                   {opt.hasIco? addIcon():""}
                   {opt.text}
@@ -51,26 +51,32 @@ const FilterSearchResults = ({ children }) => {
         </div>
       </div>
     )
-    // [&>input[type=\"range\"]: appearance-none
-    const inputStyling = ""//" [&>input[type=\"range\"]:-webkit-appearance-none  [&]:rounded-xl [&>input[type=\"range\"]::-webkit-slider-thumb]:bg-blue-300 [&>input[type=\"range\"]::-webkit-slider-thumb]:rounded-xl [&>input[type=\"range\"]::-webkit-slider-runnable-track]:h-[8px] [&>input[type=\"range\"]::-webkit-slider-runnable-track]:accent-yellow-300 [&>input[type=\"range\"]::-webkit-slider-runnable-track]:rounded-xl"  
     const RangeSelect =({data})=>{
       const [minVal, setMinVal] = useState(data.prices.min||0);
       const [maxVal, setMaxVal] = useState(data.prices.max||110);
       
       // const maxVal = useMemo(() => first, [second])
+      const handleEdit =(e)=>{
+        e.preventDefault();
+        if(e.key === "enter") setMaxVal(e.target.value) 
+      }
       return(
         <div className="range-selector-wrapper flex flex-col  whitespace-nowrap font-semibold">
           <h4>{data?.label}</h4>
           <div className="range-wrapper font-medium ">
-            <RangeSelector min={0} max={110} stateUpdaters={{ min: { val: minVal, setMinVal }, max: { val: maxVal, setMaxVal } }} onChange={({ min, max }) => { } }/>
-            <div className="current-range-values flex flex-row justify-between">
+            <div className="range-input">
+
+              <RangeSelector min={data.prices.min} max={data.prices.max} stateUpdaters={{ min: { val: minVal, setMinVal }, max: { val: maxVal, setMaxVal } }} onChange={({ min, max }) => { } }/>
+            </div>
+            <div className="current-range-values mx-2 flex flex-row justify-between">
               <div className="min-value px-2 min-w-[3.5rem] flex justify-center border-2 border-blue-500 rounded-lg ">
-                <span className="w-max" >
+                <span slot={minVal} className="w-max" >
                   {minVal}
                 </span>
               </div>
               <div className="max-value px-2 min-w-[3.5rem] flex justify-center border-2 border-blue-500 rounded-lg ">
-                <span className="w-max">
+                
+                <span slot={maxVal}  className="w-max focus:outline-none">
                   {maxVal}
                 </span>
               </div>
@@ -94,7 +100,7 @@ const FilterSearchResults = ({ children }) => {
 
         return <SelectOpt data={{label:d.label, options:d.options}}/>;
       case "rangeSelector":
-        return <RangeSelect data={{ label: d.label, prices:{min:0, max:110} }} />
+        return <RangeSelect data={{ label: d.label, prices:{min:500, max:3210} }} />
       default:
         break;
     }
@@ -110,7 +116,7 @@ const FilterSearchResults = ({ children }) => {
   }
   // const DeliveryTimeSelector
   return (
-    <div className="filter-search-results p-2 w-52 child:w-full font-[arial] " >
+    <div className="filter-search-results py-2 mx-4 w-52 flex flex-col gap-4 font-[arial]  " >
       <Categories/>
       <FilterInput inputType={ "toggle" } />
       <FilterInput inputType={ "selector" } />
